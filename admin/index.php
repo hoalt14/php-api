876bb8ffdcec29834479ab34d7983e84d5722877
Hòa <lethanhhoa.boi@gmail.com>
2020-11-12 11:30:30 +0700

    update

--- admin/index.php
+++ admin/index.php
@@ -35,42 +35,6 @@ define('BASE_UPLOAD', 'http://testphp.newpinetech.com/upload/');
 		<h4 style='color: #848484;padding: 10px 25px 10px 15px;font-size: 12px;'>MAIN NAVIGATION</h4>
 		<ul class='ul-control'>
 			<li>
-				<a href="<?php echo BASE_URL ?>?pages=quangcao" class='<?php echo @$_GET['pages'] == 'quangcao' ? 'active' : ''; ?>'>
-					<i class='fa fa-tags'></i>
-					<label>Quảng Cáo</label>
-				</a>
-			</li>
-			<li>
-				<a href="<?php echo BASE_URL ?>?pages=album" class='<?php echo @$_GET['pages'] == 'album' ? 'active' : ''; ?>'>
-					<i class='fa fa-tags'></i>
-					<label>Album</label>
-				</a>
-			</li>
-			<li>
-				<a href="<?php echo BASE_URL ?>?pages=baihat" class='<?php echo @$_GET['pages'] == 'baihat' ? 'active' : ''; ?>'>
-					<i class='fa fa-tags'></i>
-					<label>Bài Hát</label>
-				</a>
-			</li>
-			<li>
-				<a href="<?php echo BASE_URL ?>?pages=chude" class='<?php echo @$_GET['pages'] == 'chude' ? 'active' : ''; ?>'>
-					<i class='fa fa-tags'></i>
-					<label>Chủ Đề</label>
-				</a>
-			</li>
-			<li>
-				<a href="<?php echo BASE_URL ?>?pages=playlist" class='<?php echo @$_GET['pages'] == 'playlist' ? 'active' : ''; ?>'>
-					<i class='fa fa-tags'></i>
-					<label>Playlist</label>
-				</a>
-			</li>
-			<li>
-				<a href="<?php echo BASE_URL ?>?pages=theloai" class='<?php echo @$_GET['pages'] == 'theloai' ? 'active' : ''; ?>'>
-					<i class='fa fa-tags'></i>
-					<label>Thể Loại</label>
-				</a>
-			</li>
-			<li>
 				<a href="<?php echo BASE_URL ?>?pages=phong" class='<?php echo @$_GET['pages'] == 'phong' ? 'active' : ''; ?>'>
 					<i class='fa fa-tags'></i>
 					<label>Phòng</label>
--- pages/home.php
+++
@@ -1,9 +0,0 @@
-<h4 style="vertical-align: middle;
-    display: table-cell;
-">Hello admin!</h4>
-<style>
-	.wrapper {
-		display: table;
-		text-align: center;
-	}
-</style>
