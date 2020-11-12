527b528efe6dbf82f3e273801ba7f0c1f9d97f1f
Hòa <lethanhhoa.boi@gmail.com>
2020-11-12 11:29:44 +0700

    update

--- api/phong.php
+++ api/phong.php
@@ -29,6 +29,8 @@ while($row = mysqli_fetch_assoc($data)) {
 	$arrData[] = array(
 		'MaPhong'   => $row['MaPhong'],
 		'TenPhong'  => $row['TenPhong'],
+		'GiaPhong'  => $row['GiaPhong'],
+		'TinhTrang'  => $row['TinhTrang'],
 	);
 }
 
