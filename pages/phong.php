615ab6b89f9ba87b30fee31cead6c6e699908cf2
Hòa <lethanhhoa.boi@gmail.com>
2020-11-12 11:29:43 +0700

    update

--- pages/phong.php
+++ pages/phong.php
@@ -1,66 +1,54 @@
 <div class='container-fluid'>
-	<h1>PHÒNG</h1>
+	<h1>DANH SÁCH PHÒNG</h1>
 	<?php if(@$_GET['type']=='add'){ ?>
-		<?php 
+		<?php
 			if(@$_POST['submit']){								
 
-				if(@$_GET['id']){
-					$idBaiHat        = $_POST['idBaiHat'];
-					$noiDungQuangCao = $_POST['noiDungQuangCao'];	
-				
+				if(@$_GET['id']) {
+					$TenPhong = $_POST['TenPhong'];
+					$GiaPhong = $_POST['GiaPhong'];	
 
-					$result = $conn->query("SELECT * FROM quangcao WHERE idQuangCao = ".$_GET['id']." LIMIT 1");
+					$result = $conn->query("SELECT * FROM PHONG WHERE MaPhong = ".$_GET['id']." LIMIT 1");
 					$row = $result->fetch_assoc();
 
-					$update = array(
-						"idBaiHat = $idBaiHat",
-						"noiDungQuangCao = '$noiDungQuangCao'",
-					);					
-
-					if(!empty($_FILES['hinhQuangCao']['name'])){
-						$hinhQuangCao    = time().".png";
-						$update[] = "hinhQuangCao = '$hinhQuangCao'";
+					if(!empty($_FILES['TinhTrang']['name'])){
+						$TinhTrang    = time().".png";
+						$sql = "UPDATE album SET TenPhong='$TenPhong', GiaPhong = '$GiaPhong', TinhTrang = '$TinhTrang' WHERE MaPhong=".$row['MaPhong'];
+					}else{
+						$sql = "UPDATE album SET TenPhong='$TenPhong', GiaPhong = '$GiaPhong' WHERE MaPhong=".$row['MaPhong'];
 					}
-
-					$update = implode(', ', $update);
-					$sql = "UPDATE quangcao SET $update WHERE idQuangCao=".$row['idQuangCao'];
-										
+					
 					if ($conn->query($sql) === TRUE) {
-						if(!empty($_FILES['hinhQuangCao']['name'])){
-							move_uploaded_file($_FILES['hinhQuangCao']['tmp_name'], '../upload/quangcao/'. time().'.png');
-							@unlink('../upload/quangcao/'.$row['hinhQuangCao']);
+						if(!empty($_FILES['TinhTrang']['name'])){
+							move_uploaded_file($_FILES['TinhTrang']['tmp_name'], '../upload/album/'. time().'.png');
+							@unlink('../upload/album/'.$row['TinhTrang']);
 						}
-
-
-				     	echo "<script>alert('Cập nhật QuangCao thành công');</script>";
-					    echo "<script>window.location='".BASE_URL."?pages=quangcao&type=add&id=".$row['idQuangCao']."'</script>";
+				     	echo "<script>alert('Cập nhật Album thành công');</script>";
+					    echo "<script>window.location='".BASE_URL."?pages=album&type=add&id=".$row['MaPhong']."'</script>";
 					} else {
 						echo "Error updating record: " . $conn->error;
 					    echo "<script>alert('Có lỗi xảy ra, vui lòng thử lại');</script>";
-					    echo "<script>window.location='".BASE_URL."?pages=quangcao&type=add&id=".$row['idQuangCao']."'</script>";
+					    echo "<script>window.location='".BASE_URL."?pages=album&type=add&id=".$row['MaPhong']."'</script>";
 					}
 				}else{
-					$idBaiHat        = $_POST['idBaiHat'];
-					$noiDungQuangCao = $_POST['noiDungQuangCao'];
+					$TenPhong     = $_POST['TenPhong'];
+					$GiaPhong = $_POST['GiaPhong'];	
+					if(!empty($_FILES['TinhTrang']['name']))
+						$TinhTrang    = time().".png";	
+					else $TinhTrang='';
 
-
-					if(!empty($_FILES['hinhQuangCao']['name']))
-						$hinhQuangCao    = time().".png";	
-					else $hinhQuangCao='';
-
-					$sql = "INSERT INTO quangcao (idBaiHat, noiDungQuangCao, hinhQuangCao)
-					VALUES ($idBaiHat, '$noiDungQuangCao', '$hinhQuangCao')";
+					$sql = "INSERT INTO album (TenPhong, GiaPhong, TinhTrang)
+					VALUES ('$TenPhong', '$GiaPhong', '$TinhTrang')";
 					if ($conn->query($sql) === TRUE) {
 					    $last_id = $conn->insert_id;
-					    if(!empty($_FILES['hinhQuangCao']['name']))
-	                    	move_uploaded_file($_FILES['hinhQuangCao']['tmp_name'], '../upload/quangcao/'. $hinhQuangCao);					   
-	                 
-					    echo "<script>alert('Thêm QuangCao thành công');</script>";
-					    echo "<script>window.location='".BASE_URL."?pages=quangcao&type=add&id=".$last_id."'</script>";
+					    if(!empty($_FILES['TinhTrang']['name']))
+	                    	move_uploaded_file($_FILES['TinhTrang']['tmp_name'], '../upload/album/'. $TinhTrang);
+					    echo "<script>alert('Thêm Album thành công');</script>";
+					    echo "<script>window.location='".BASE_URL."?pages=album&type=add&id=".$last_id."'</script>";
 					} else {
 						// echo "Error: " . $sql . "<br>" . $conn->error;
 					    echo "<script>alert('Có lỗi xảy ra, vui lòng thử lại');</script>";
-					    echo "<script>window.location='".BASE_URL."?pages=quangcao&type=add'</script>";
+					    echo "<script>window.location='".BASE_URL."?pages=album&type=add'</script>";
 					}
 				}
 				$conn->close();
@@ -68,11 +56,9 @@
 
 			if(@$_GET['id']){
 				$id = @$_GET['id'];
-				$result = $conn->query("SELECT * FROM quangcao WHERE idQuangCao = $id LIMIT 1");
+				$result = $conn->query("SELECT * FROM album WHERE MaPhong = $id LIMIT 1");
 				$row = $result->fetch_assoc();
 			}
-
-
 			
 		?>
 		<div class='box'>
@@ -80,49 +66,37 @@
 				THÊM MỚI ALBUM
 				<button type="button" class="btn btn-default pull-right" 
 					style="position: relative;top: -7px;"
-					onclick="window.location='<?php echo BASE_URL ?>?pages=quangcao'" 
+					onclick="window.location='<?php echo BASE_URL ?>?pages=album'" 
 				>QUAY LẠI</button>
 			</div>
 			<div class='body'>
-				<form action="<?php echo BASE_URL ?>?pages=quangcao&type=add&id=<?php echo @$row['idQuangCao']?>" method="POST" class="form-horizontal" role="form" enctype="multipart/form-data">
-
+				<form action="<?php echo BASE_URL ?>?pages=album&type=add&id=<?php echo @$row['MaPhong']?>" method="POST" class="form-horizontal" role="form" enctype="multipart/form-data">
 					<div class="form-group">
-						<label for="input" class="col-sm-2 control-label">Bài Hát</label>
+						<label for="input" class="col-sm-2 control-label">Tên Album</label>
 						<div class="col-sm-10">
-							<select name="idBaiHat" id="input" class="form-control">
-								<option value="">-- Bài Hát --</option>
-								<?php 
-									$result = $conn->query("SELECT * FROM baihat");
-									if ($result->num_rows > 0) {
-									    while($al = $result->fetch_assoc()) {
-									    	echo"<option value='$al[idBaiHat]' ".($row['idBaiHat'] == $al['idBaiHat']?'selected':'').">$al[tenBaiHat]</option>";
-									    }
-									}
-									$conn->close();
-								?>				
-							</select>						
+							<input type="text" name="TenPhong" class="form-control" value="<?php echo @$row['TenPhong']?>" required="required">
 						</div>
 					</div>	
 
 					<div class="form-group">
-						<label for="input" class="col-sm-2 control-label">Nội Dung Quảng Cáo</label>
+						<label for="input" class="col-sm-2 control-label">Tên Ca sỉ Album</label>
 						<div class="col-sm-10">
-							<textarea name="noiDungQuangCao" class="form-control" rows="3" required="required"><?php echo @$row['noiDungQuangCao']?></textarea>						
+							<input type="text" name="GiaPhong" class="form-control" value="<?php echo @$row['GiaPhong']?>" required="required">
 						</div>
 					</div>	
 
 					<div class="form-group">
-						<label for="input" class="col-sm-2 control-label">Hình Quảng Cáo</label>
+						<label for="input" class="col-sm-2 control-label">Hình Album</label>
 						<div class="col-sm-10">
-							<input type="file" name="hinhQuangCao" class="form-control">
+							<input type="file" name="TinhTrang" class="form-control">
 						</div>
 					</div>
 					<?php 
-						if(@$row['hinhQuangCao']){
+						if(@$row['TinhTrang']){
 							echo '<div class="form-group">
 									<label for="input" class="col-sm-2 control-label"></label>
 									<div class="col-sm-10">
-										<img src="'.BASE_UPLOAD.'quangcao/'.$row['hinhQuangCao'].'" style="width:100px">
+										<img src="'.BASE_UPLOAD.'album/'.$row['TinhTrang'].'" style="width:100px">
 									</div>
 								</div>';
 						}
@@ -141,17 +115,17 @@
 	
 	<?php }elseif(@$_GET['type']=='del'){ 
 		$id = @$_GET['id'];
-		$result = $conn->query("SELECT * FROM quangcao WHERE idQuangCao = $id LIMIT 1");
+		$result = $conn->query("SELECT * FROM album WHERE MaPhong = $id LIMIT 1");
 		$row = $result->fetch_assoc();
 		if(!empty($row)){
-			$sql = "DELETE FROM quangcao WHERE idQuangCao=$id";
+			$sql = "DELETE FROM album WHERE MaPhong=$id";
 			if ($conn->query($sql) === TRUE) {
-				@unlink('../upload/quangcao/'.$row['hinhQuangCao']);
+				@unlink('../upload/album/'.$row['TinhTrang']);
 			    echo "<script>alert('Xóa thành công');</script>";
-			    echo "<script>window.location='".BASE_URL."?pages=quangcao'</script>";
+			    echo "<script>window.location='".BASE_URL."?pages=album'</script>";
 			} else {
 			    echo "<script>alert('Xóa thất bại');</script>";
-			    echo "<script>window.location='".BASE_URL."?pages=quangcao'</script>";
+			    echo "<script>window.location='".BASE_URL."?pages=album'</script>";
 			}
 		}		
 		$conn->close();
@@ -159,10 +133,10 @@
 	}else{ ?>
 		<div class='box'>
 			<div class='header'>
-				DANH SÁCH BÀI HÁT
+				DANH SÁCH ALBUM
 				<button type="button" class="btn btn-default pull-right" 
 					style="position: relative;top: -7px;"
-					onclick="window.location='<?php echo BASE_URL ?>?pages=quangcao&type=add'" 
+					onclick="window.location='<?php echo BASE_URL ?>?pages=album&type=add'" 
 				>THÊM MỚI</button>
 			</div>
 			<div class='body'>
@@ -171,25 +145,25 @@
 						<tr>
 							<th>ID</th>	
 							<th>Hình</th>							
-							<th>Nội Dung</th>
-							<th>BaiHat</th>
+							<th>Tên Album</th>
+							<th>Tên Ca sĩ</th>
 							<th>Sửa</th>
 							<th>Xóa</th>
 						</tr>
 					</thead>
 					<tbody>
 						<?php 
-							$result = $conn->query("SELECT * FROM quangcao");
+							$result = $conn->query("SELECT * FROM album");
 							if ($result->num_rows > 0) {
 							    while($row = $result->fetch_assoc()) {
 							    	echo"<tr>
-							    			<td style='width:30px'>$row[idQuangCao]</td>
-											<td style='width:100px'><img src='".BASE_UPLOAD."quangcao/$row[hinhQuangCao]' class='img-imge'></td>
+							    			<td>$row[MaPhong]</td>
+											<td><img src='".BASE_UPLOAD."album/$row[TinhTrang]' class='img-imge'></td>
 											
-											<td>$row[noiDungQuangCao]</td>
-											<td style='width:50px'>$row[idBaiHat]</td>
-											<td style='width:30px'><a href='".BASE_URL."?pages=quangcao&type=add&id=$row[idQuangCao]'>Sửa</a></td>
-											<td style='width:30px'><a href='".BASE_URL."?pages=quangcao&type=del&id=$row[idQuangCao]'>Xóa</a></td>
+											<td>$row[TenPhong]</td>
+											<td>$row[GiaPhong]</td>
+											<td><a href='".BASE_URL."?pages=album&type=add&id=$row[MaPhong]'>Sửa</a></td>
+											<td><a href='".BASE_URL."?pages=album&type=del&id=$row[MaPhong]'>Xóa</a></td>
 										</tr>";
 							    }
 							}
@@ -200,16 +174,7 @@
 			</div>
 		</div>
 		<script type="text/javascript">
-			$('.table').dataTable({
-				"order": [[ 0, "desc" ]],
-				"aoColumns":[		
-					{"bSortable": true},
-					{"bSortable": false},
-					{"bSortable": true},
-					{"bSortable": false},
-					{"bSortable": false},					
-				],
-			});		
+			$('.table').dataTable();	
 		</script>
 	<?php }?>
 </div>
